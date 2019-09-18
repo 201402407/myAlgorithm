69a66768caf992ea542b218ce9c79a7bb8251394
201402407 <banner4@cnu.ac.kr>
2019-09-19 01:26:03 +0900

    InsertionSort 클래스 파일 인풋 구현

--- AlgorithmHomework/src/week1/InsertionSort.java
+++ AlgorithmHomework/src/week1/InsertionSort.java
@@ -1,8 +1,48 @@
 // 201402407 이해원
 package week1;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 // 삽입정렬 구현하기
 public class InsertionSort {
-
+	static int sort[];
+	public static void main(String args[]) {
+		try { 
+			String inputFileSrc = new java.io.File("").getAbsolutePath() + "/src/input.txt";	// 상대 경로 설정
+			FileInputStream fileInputStream = new FileInputStream(inputFileSrc);
+			
+			byte[] buffer = new byte[fileInputStream.available()];	// 파일의 전체 크기만큼 바이트 버퍼 설정
+			while(fileInputStream.read(buffer) != -1) {}	// 버퍼에 값 저장
+			StringTokenizer st = new StringTokenizer(new String(buffer)); // 토큰으로 숫자 분리
+	    	
+			int countTokens = st.countTokens();	// 갯수 넣기
+	    	sort = new int[countTokens];
+	    	
+		    for(int i = 0; i < countTokens; i++) {
+			    int number = Integer.parseInt(st.nextToken());
+			    sort[i] = number;
+		    }
+		    
+		    insertionSort();
+			
+		}
+		catch(NumberFormatException e ) {
+			System.out.println("숫자가 아닌 문자를 입력받았습니다.");
+			System.exit(0);
+		}
+		catch(FileNotFoundException e) {
+			System.out.println("해당 경로에 인풋 파일이 존재하지 않습니다.");
+			System.exit(0);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private static void insertionSort() {
+		
+	}
 }
