970aaf16c27952ee0a87a49cd1d577903ae40cb9
201402407 <banner4@cnu.ac.kr>
2019-09-28 03:36:39 +0900

    백준 알고리즘 1157번 문제 해결. 주석 추가

--- backjoonAlgorithm/src/anything/p1157.java
+++ backjoonAlgorithm/src/anything/p1157.java
@@ -2,6 +2,8 @@ package anything;
 
 import java.io.IOException;
 
+// 단어 공부
+// 문자열 문제
 public class p1157 {
 	public static void main(String args[]) throws IOException {
 //		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
