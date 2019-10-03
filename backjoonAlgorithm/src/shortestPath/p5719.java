ea7a7c93eb900c6d3e0474051b5012a767dae0d4
LeeHaeWon <banner4@cnu.ac.kr>
2019-10-03 19:11:07 +0900

    백준 5719번 문제 진행 중.. 그래프에서 제거하는 방식 틀림.

--- backjoonAlgorithm/src/shortestPath/p5719.java
+++ backjoonAlgorithm/src/shortestPath/p5719.java
@@ -13,6 +13,7 @@ import java.util.StringTokenizer;
 public class p5719 {
 	static StringBuilder sb = new StringBuilder();
 	static List<Vertex>[] graph;
+	static int[] distance;
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str;
@@ -24,7 +25,7 @@ public class p5719 {
 				break;
 			// 초기 설정
 			graph = new ArrayList[n];
-			int[] distance = new int[n];
+			distance = new int[n];
 			boolean[] visited = new boolean[n];	// 거의 최단 경로이기 때문에 사용안할수도있다.
 			for(int i = 0; i < n; i++) {
 				graph[i] = new ArrayList<Vertex>();
@@ -43,86 +44,109 @@ public class p5719 {
 				int weight = Integer.valueOf(st.nextToken());
 				graph[from].add(new Vertex(to, weight));
 			}
-//			dijkstra(start, end, distance, visited, graph);// 다익스트라 탐색 시작
+			find(start, end, n);
 		}
 		System.out.println(sb.toString());
 	}
 	
+	// 최단 경로 다 없애야 하나
 	private static void find(int startPoint, int endPoint, int n) {
-		int result = dijkstra(startPoint, endPoint, n);
+		int[] result = dijkstra(startPoint, endPoint, n);	// endPoint까지의 최단 경로 추출
+		if(!deletePath(startPoint, endPoint, result)) {
+			sb.append("-1").append("\n");
+			return;
+		}
+		int minDistance = distance[endPoint];
+		do {
+			result = dijkstra(startPoint, endPoint, n);	// 재 탐색
+			if(!deletePath(startPoint, endPoint, result)) {
+				sb.append("-1").append("\n");
+				return;
+			}
+		}
+		while(minDistance == distance[endPoint]);
+		sb.append(distance[endPoint]).append("\n");
+	}
+	
+	// 경로 삭제 -> 삭제함 : true. 삭제 못함 : false
+	private static boolean deletePath(int startPoint, int endPoint, int[] result) {
+		// 경로 삭제
+		int endVertex = endPoint;
+		while(endVertex != startPoint) {
+			int startVertex = result[endVertex];
+
+			if(startVertex == -1) { // 목적지점까지 가는 경로가 없을 경우
+				return false;
+			}
+			boolean isValid = false; // 그래프 내에 이어지는 Vertex가 존재하는 지 체크
+			for(Vertex v : graph[startVertex]) {
+				if(v.getVertex() == endVertex) {
+					graph[startVertex].remove(v);
+					endVertex = startVertex;
+					isValid = true;
+					break;
+				}
+			}
+			if(!isValid)
+				return false;
+		}
+		return true;
 	}
 	
 	// 처음에 싹 돌려서 나온 최단 경로를 
-	private static int dijkstra(int startPoint, int endPoint, int n) {
+	private static int[] dijkstra(int startPoint, int endPoint, int n) {
 		PriorityQueue<Vertex> queue = new PriorityQueue<>();	// comparable로 객체 우선순위 바꿈
 		int answer = Integer.MAX_VALUE;
-//		boolean isFirst = true;
-		int[] distance = new int[n];
 		boolean[] visited = new boolean[n];
-		Arrays.fill(distance, Integer.MAX_VALUE);
 //		List<Integer> result = new ArrayList<Integer>();
+		int[] result = new int[n];	// 최단 경로
+		Arrays.fill(distance, Integer.MAX_VALUE);
+		Arrays.fill(result, -1);
+		distance[startPoint] = 0;
 		queue.offer(new Vertex(startPoint, distance[startPoint]));	// 출발점 넣기
 		
 		while(!queue.isEmpty()) { 
 			Vertex startVertex = queue.poll();
 			startPoint = startVertex.getVertex();
-//			graph[startPoint].remove(vertex);
-			if(startPoint == endPoint) {
-				return distance[endPoint];
-			}
 			if(visited[startPoint])
 				continue;
 			visited[startPoint] = true;
-		
+			
 			for(Vertex vertex : graph[startPoint]) {
 				int endVertex = vertex.getVertex();
 				if(distance[endVertex] > distance[startPoint] + vertex.getWeight()) {
 					distance[endVertex] = distance[startPoint] + vertex.getWeight();
 					queue.offer(new Vertex(endVertex, distance[endVertex]));
-					
-					
+					result[endVertex] = startPoint;	// 해당 경로의 마지막 출력
 				}
 			}
-			
-//			for(Vertex vertex : graph[startPoint]) {
-//				int endVertex = vertex.getVertex();
-//				if(distance[endVertex] > distance[startPoint] + vertex.getWeight()) {
-//					if(endPoint == endVertex) {
-//						if(!isFirst)
-//							answer = distance[endVertex];
-//						else
-//							isFirst = false;
-//					}
-//					distance[endVertex] = distance[startPoint] + vertex.getWeight();
-//					
-//					queue.offer(new Vertex(endVertex, distance[endVertex]));
-//				}
-//				else if(distance[endVertex] == distance[startPoint] + vertex.getWeight()) {
-//					continue;
-//				}
-//				else {
-//					System.out.println("endPoint : " + endPoint + " , endVertex : " + endVertex);
-//					if(endPoint == endVertex) {
-//						System.out.println("before : " + answer);
-//						answer = Math.min(answer, distance[startPoint] + vertex.getWeight());
-//						System.out.println("after : " + answer);
-//					}
-//				}
-//			}
 		}
-		if(answer == Integer.MAX_VALUE)
-			answer = -1;
-		sb.append(answer).append("\n");
+		return result;
 	}
 }
 
 class Vertex implements Comparable<Vertex> {
+	private int startVertex;
 	private int vertex;
 	private int weight;
 	
 	Vertex(int vertex, int weight) {
 		this.vertex = vertex;
 		this.weight = weight;
+	}
+	
+	Vertex(int vertex, int weight, int startVertex) {
+		this.vertex = vertex;
+		this.weight = weight;
+		this.startVertex = startVertex;
+	}
+	
+	public void setStartVertex(int startVertex) {
+		this.startVertex = startVertex;
+	}
+	
+	public int getStartVertex() {
+		return this.startVertex;
 	}
 	
 	public int getVertex() {
@@ -147,6 +171,4 @@ class Vertex implements Comparable<Vertex> {
 		else
 			return 1;
 	}
-	
-	
 }
