197e259b129b46c5cd038479622d2ba6e0219eb4
LeeHaeWon <banner4@cnu.ac.kr>
2019-10-09 14:44:04 +0900

    백준 2156번 문제 해결. 포도주.. DP 요구 사항을 잘 생각해서 점화식을 세우자 진짜.

--- backjoonAlgorithm/src/dynamic/p2156.java
+++ backjoonAlgorithm/src/dynamic/p2156.java
@@ -6,29 +6,35 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 // 포도주 시식
+// 연속 3잔은 X
+// 연속 0잔 -> result[n] = max(result[n], result[n-1])
+// 연속 1잔 -> result[n] = result[n-2] + wine[n]; -> 이전 잔은 무조건 안마시고, 이전이전잔은 상관 없음
+// 연속 2잔 -> result[n] = result[n-3] + wine[n-1] + wine[n]; -> 이전잔과 현재잔은 무조건 마시고, 이전이전잔은 무조건 안미셔야함
 public class p2156 {
 	static int[] wine;
 	static int[] result;
-	static int[][] answer;
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.valueOf(st.nextToken());
-		wine = new int[n];
-		result = new int[n];
-		answer = new int[n][2];
-		for(int i = 0; i < n; i++) {
+		wine = new int[n + 1];
+		result = new int[n + 1];
+		for(int i = 1; i <= n; i++) {
 			st = new StringTokenizer(br.readLine());
 			int weight = Integer.valueOf(st.nextToken());
 			wine[i] = weight;
 		}
 	
-		answer[0][0] = wine[0];
-		answer[0][1] = 0;
-		int max = wine[0];
+		result[1] = wine[1];
+		
         if(n >= 2) {
-            answer[1][0] = wine[1];
-		    answer[1][1] = answer[0][0] + wine[1];
-        }	
+        	result[2] = result[1] + wine[2];
+        }
+        for(int i = 3; i <= n; i++) {
+        	result[i] = Math.max(result[i - 2] + wine[i], result[i - 3] + wine[i - 1] + wine[i]);
+        	result[i] = Math.max(result[i], result[i - 1]);
+        }
+        
+        System.out.println(result[n]);
 	}
 }
