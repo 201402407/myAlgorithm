7e4389a83acedba5d7d67f9d1744a2b38f4a3f7b
LeeHaeWon <banner4@cnu.ac.kr>
2019-10-27 19:35:22 +0900

    7주차 알고리즘 과제 파일 생성 및 주석 추가

---
+++ AlgorithmHomework/src/week7/SLS.java
@@ -0,0 +1,15 @@
+package week7;
+
+/*
+ * 7주차 과제
+ * Segmented Least Squares
+ * 최소 제곱법
+ * 참값과 근사값의 오차의 제곱합이 최소가 되게 함
+ * 데이터의 분포를 최대한 잘 근사하는 직선을 찾는 일
+ * 직선이 여러 개일 수 있음.
+ */
+public class SLS {
+	public static void main(String args[]) {
+		
+	}
+}
