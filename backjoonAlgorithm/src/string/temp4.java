529a17c1f77a7555ce7804c2a5363644b8999dad
Leehaewon <34370187+201402407@users.noreply.github.com>
2020-05-19 18:10:16 +0900

    Update temp4.java

--- backjoonAlgorithm/src/string/temp4.java
+++ backjoonAlgorithm/src/string/temp4.java
@@ -8,18 +8,21 @@ import java.util.StringTokenizer;
 // 11586
 public class temp4 {
 	static char[][] map;
+	static String[] mirror;
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int n = Integer.valueOf(st.nextToken());
 		map = new char[n][n];
+		mirror = new String[n];
 		
 		// 맵 input 받아오기
 		for(int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 			String line = st.nextToken();
-			map[i] = line.toCharArray();
+			mirror[i] = line;
+//			map[i] = line.toCharArray();
 		}
 		
 		st = new StringTokenizer(br.readLine());
@@ -29,25 +32,30 @@ public class temp4 {
 		// 거울 그리기
 		if(k == 1) {
 			for(int i = 0; i < n; i++) {
-				sb.append(map[i].toString());
+//				for(int j = 0; j < n; j++) {
+//					sb.append(map[i][j]);
+//				}
+				sb.append(mirror[i]);
 				if(i + 1 < n) 
 					sb.append("\n");
 			}
 		}
 		if(k == 2) {
 			for(int i = 0; i < n; i++) {
-				for(int j = n - 1; j >= 0; j--) {
-					sb.append(map[i][j]);
-				}
+				sb.append(new StringBuffer(mirror[i]).reverse().toString());
+//				for(int j = n - 1; j >= 0; j--) {
+//					sb.append(map[i][j]);
+//				}
 				if(i + 1 < n) 
 					sb.append("\n");
 			}
 		}
 		if(k == 3) {
 			for(int i = n - 1; i >= 0; i--) {
-				for(int j = 0; j < n; j++) {
-					sb.append(map[i][j]);
-				}
+				sb.append(mirror[i]);
+//				for(int j = 0; j < n; j++) {
+//					sb.append(map[i][j]);
+//				}
 				if(i > 0) 
 					sb.append("\n");
 			}
