cdf21048c1dc38d20566257a48ce866f394e2897
Leehaewon <34370187+201402407@users.noreply.github.com>
2020-09-09 23:46:52 +0900

    백준 10824번 [구현] - [네 수] 문제 해결

---
+++ backjoonAlgorithm/src/realization/p10824.java
@@ -0,0 +1,19 @@
+package temp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 네 수
+// 구현
+public class p10824 {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long ab = Long.valueOf(st.nextToken() + st.nextToken());
+		long cd = Long.valueOf(st.nextToken() + st.nextToken());
+		System.out.println(ab + cd);
+	}
+}
