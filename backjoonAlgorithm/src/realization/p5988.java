65fd46cf968f67aafc4bfb02c9e9f431b505f3f4
Leehaewon <34370187+201402407@users.noreply.github.com>
2020-09-10 23:28:15 +0900

    백준 5988번 [구현] - [홀수일까 짝수일까] 문제 해결

---
+++ backjoonAlgorithm/src/realization/p5988.java
@@ -0,0 +1,49 @@
+package temp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// È¦¼öÀÏ±î Â¦¼öÀÏ±î 
+// ±¸Çö 
+public class p5988 {
+	public static void main(String args[]) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.valueOf(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while(n --> 0) {
+			String str = br.readLine();
+			if(isOdd(str)) {
+				sb.append("odd");
+			}
+			else {
+				sb.append("even");
+			}
+			
+			sb.append("\n");
+		}
+		
+		System.out.print(sb.toString());
+	}
+	
+	static boolean isOdd(String str) {
+		char ch = str.charAt(str.length() - 1);
+		switch(ch) {
+		case '0':
+		case '2':
+		case '4':
+		case '6':
+		case '8':
+			return false;
+		case '1':
+		case '3':
+		case '5':
+		case '7':
+		case '9':
+			return true;
+		}
+		
+		return false;
+	}
+}
