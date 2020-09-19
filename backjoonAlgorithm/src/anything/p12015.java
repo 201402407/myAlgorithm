a86c3b99aba3629242483f5e8b45de4a5948848b
Leehaewon <banner4@o.cnu.ac.kr>
2020-09-19 17:06:29 +0900

    백준 12015번 수정

--- backjoonAlgorithm/src/anything/p12015.java
+++ backjoonAlgorithm/src/anything/p12015.java
@@ -1,25 +1,25 @@
 package anything;
 
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.InputStream;
+import java.util.InputMismatchException;
 
 // 가장 긴 증가하는 부분 수열 2
 // LIS + 이분탐색 
 public class p12015 {
 	public static void main(String args[]) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.valueOf(br.readLine());
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		InputReader in = new InputReader(System.in);
+		int n = in.readInt();
+//		int n = Integer.valueOf(br.readLine());
 		int[] arr = new int[n];
         // 처음 값 설정 
-		StringTokenizer st = new StringTokenizer(br.readLine());
+//		StringTokenizer st = new StringTokenizer(br.readLine());
 		
         // 입력 받자마자 계산하기
 		for(int i = 0; i < n; i++) {
-			int ele = Integer.valueOf(st.nextToken());
-			arr[i] = ele;
+//			int ele = Integer.valueOf(st.nextToken());
+			arr[i] = in.readInt();
 		}
 		
 		System.out.println(binarySearch(arr, n));
@@ -56,5 +56,69 @@ public class p12015 {
 		}
 		
 		return dpLen;
+	}
+	
+	// INPUT 속도 증가
+	private static class InputReader {
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int numChars;
+		private SpaceCharFilter filter;
+
+		public InputReader(InputStream stream) {
+			this.stream = stream;
+		}
+
+		public int read() {
+			if (numChars == -1) {
+				throw new InputMismatchException();
+			}
+			if (curChar >= numChars) {
+				curChar = 0;
+				try {
+					numChars = stream.read(buf);
+				} catch (IOException e) {
+					throw new InputMismatchException();
+				}
+				if (numChars <= 0) {
+					return -1;
+				}
+			}
+			return buf[curChar++];
+		}
+
+		public int readInt() {
+			int c = read();
+			while (isSpaceChar(c)) {
+				c = read();
+			}
+			int sgn = 1;
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			int res = 0;
+			do {
+				if (c < '0' || c > '9') {
+					throw new InputMismatchException();
+				}
+				res *= 10;
+				res += c - '0';
+				c = read();
+			} while (!isSpaceChar(c));
+			return res * sgn;
+		}
+
+		public boolean isSpaceChar(int c) {
+			if (filter != null) {
+				return filter.isSpaceChar(c);
+			}
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+
+		public interface SpaceCharFilter {
+			public boolean isSpaceChar(int ch);
+		}
 	}
 }
