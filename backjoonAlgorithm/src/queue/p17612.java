77f18f4b80e5a54f4c2a2f0a901ee6a8bc44ed12
201402407 <banner4@naver.com>
2021-07-19 01:56:22 +0900

    백준 17612번 문제 해결 중

--- backjoonAlgorithm/src/queue/p17612.java
+++ backjoonAlgorithm/src/queue/p17612.java
@@ -3,7 +3,9 @@ package queue;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -11,9 +13,10 @@ import java.util.StringTokenizer;
 // 쇼핑몰
 // 우선순위 큐
 public class p17612 {
-	static int n, k;
-	static Queue<Customer> customerQueue = new LinkedList<Customer>();
-	static PriorityQueue<Counter> counterQueue = new PriorityQueue<Counter>();
+	static int n, k, nowTime;	// nowTime : 개점한 후 지난 시간
+	static Queue<Customer> customerQueue = new LinkedList<Customer>();	// 현재 대기중인 고객 큐
+	static PriorityQueue<Counter> counterQueue = new PriorityQueue<Counter>();	// 현재 카운터 진행상태 우선순위큐
+	static List<Integer> outCustomerTimeList = new ArrayList<Integer>();
 	
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -34,16 +37,58 @@ public class p17612 {
 			if(customerQueue.isEmpty()) {	// 줄서있는 고객보다 계산대 수가 많은 경우
 				break;
 			}
-			
-			counterQueue.offer(new Counter());
+			// 2-1) 고객 큐에서 K만큼 꺼내기
+			Customer customer = customerQueue.poll();
+			counterQueue.offer(new Counter(i, customer.id, customer.w));
 		}
-//		2-1) 고객 큐에서 K만큼 꺼내기
-//		3) 큐가 empty될 때까지 while문 반복수행하는데
-//		3-1) 비어있는 계산대 우선순위 큐를 만들어서 계산대 큐에서 꺼내는 순간 계산대 번호 넣기 
-//		4) 계산대 큐에서 가장 걸리는시간이 짧은거 꺼내기
-//		4-1) 회원번호 ArrayList에 담기
 		
+		PriorityQueue<Integer> waitedCustomerQueue = new PriorityQueue<Integer>();
 		
+		// 3) 큐가 empty될 때까지 while문 반복수행하는데
+		while(!counterQueue.isEmpty()) {
+			// 3-1) 비어있는 계산대 우선순위 큐를 만들어서 계산대 큐에서 꺼내는 순간 계산대 번호 넣기 
+			// 4) 계산대 큐에서 가장 걸리는시간이 짧은거 꺼내기
+			if(!customerQueue.isEmpty()) {
+				Counter counter = counterQueue.poll();
+				// 5) 시간 지났으니까 개장 후 시간 갱신
+				nowTime = Math.max(nowTime, counter.time);
+				waitedCustomerQueue.offer(counter.index);
+				// 회원번호 ArrayList에 담기
+				outCustomerTimeList.add(counter.id);
+				while(!counterQueue.isEmpty()) {	// 나오는 시간이 같은 것들이 있을 경우, 가장 계산대 번호가 낮은 계산대부터 투입시키기
+					if(counterQueue.peek().time == nowTime) {
+						Counter tempCounter = counterQueue.poll();
+						waitedCustomerQueue.offer(tempCounter.index);
+						outCustomerTimeList.add(counter.id);
+						continue;
+					}
+					
+					break;
+				}
+
+				// 6) 새로운 고객 시간 계산해서 계산대에 넣기
+				while(!waitedCustomerQueue.isEmpty()) {
+					Customer newCustomer = customerQueue.poll();
+					int nextIndex = waitedCustomerQueue.poll();
+					Counter newCounter = new Counter(nextIndex, newCustomer.id, nowTime + newCustomer.w);
+					counterQueue.offer(newCounter);
+				}
+
+				continue;
+			}
+			
+			Counter counter = counterQueue.poll();
+			// 4-1) 회원번호 ArrayList에 담기
+			outCustomerTimeList.add(counter.id);
+		}
+		
+		long result = 0;
+		for(int i = 1; i <= outCustomerTimeList.size(); i++) {
+			System.out.println(outCustomerTimeList.get(i - 1));
+			result += i * outCustomerTimeList.get(i - 1);
+		}
+		
+		System.out.println(result);
 	}
 }
 
@@ -59,10 +104,17 @@ class Customer {
 
 class Counter implements Comparable<Counter> {
 	int index;
+	int id;
 	int time;
 	
 	Counter(int index, int time) {
 		this.index = index;
+		this.time = time;
+	}
+	
+	Counter(int index, int id, int time) {
+		this.index = index;
+		this.id = id;
 		this.time = time;
 	}
 
