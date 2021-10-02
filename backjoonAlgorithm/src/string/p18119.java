2c6d6c3d81a0b93aa0af2e7f013dc69ae3ce162a
201402407 <banner4@naver.com>
2021-10-02 17:44:29 +0900

    백준 18119번 [단어암기] - [비트마스킹] 문제 해결

--- backjoonAlgorithm/src/string/p18119.java
+++ backjoonAlgorithm/src/string/p18119.java
@@ -11,7 +11,8 @@ public class p18119 {
 	static final int ALPHABET_COUNT = 26;
 	static final int A = 'a';
 	static int n, m;
-	static int[][] alphabetCountArr;	// 각 문자 별 기억하는 알파벳 별 개수 저장
+	static int[][] alphabetCountArr;	// 각 문자 별 현재 기억하는 알파벳 별 개수 저장
+	static int[][] nowAlphabetCountArr;	// 각 문자 별 기억하는 알파벳 별 개수 저장
 	static int[] rememberCount;	// 각 문자 별 기억하는 알파벳 개수
 	static int[] nowCount;	// 각 문자 별 현재 기억하는 알파벳 개수
 	
@@ -21,6 +22,7 @@ public class p18119 {
 		n = Integer.valueOf(st.nextToken());
 		m = Integer.valueOf(st.nextToken());
 		alphabetCountArr = new int[n][ALPHABET_COUNT];
+		nowAlphabetCountArr = new int[n][ALPHABET_COUNT];
 		rememberCount = new int[n];
 		nowCount = new int[n];
 		
@@ -29,35 +31,55 @@ public class p18119 {
 			for(int j = 0; j < str.length(); j++) {
 				char ch = str.charAt(j);
 				alphabetCountArr[i][ch - A]++;
+				nowAlphabetCountArr[i][ch - A]++;
 			}
 			
 			rememberCount[i] = str.length();
 			nowCount[i] = str.length();
 		}
 		
+		StringBuilder sb = new StringBuilder();
 		while(m --> 0) {
 			st = new StringTokenizer(br.readLine());
 			int o = Integer.valueOf(st.nextToken());
 			String x = st.nextToken();
 			int xInt = x.charAt(0) - A;
-			
-			
+			int result = o == 1 ? forget(xInt) : remember(xInt);	// 쿼리는 누적 적용되니까
+			sb.append(result).append("\n");
 		}
+		
+		System.out.println(sb.toString());
 	}
 	
 	// 잊어버릴 때
-	static void forget(int xInt) {
+	static int forget(int xInt) {
 		// 모든 알파벳 조지기
+		int result = 0;
 		for(int i = 0; i < n; i++) {
-			if(nowCount[i] == b)
+			if(nowAlphabetCountArr[i][xInt] > 0) { // 잊을 게 있으면
+				nowAlphabetCountArr[i][xInt]--;
+				nowCount[i]--;
+			}
+			
+			result += rememberCount[i] == nowCount[i] ? 1 : 0;
 		}
+		
+		return result;
 	}
 	
 	// 기억할 때
-	static void remember(int xInt) {
+	static int remember(int xInt) {
 		// 모든 알파벳 조지기
+		int result = 0;
 		for(int i = 0; i < n; i++) {
-			if()
+			if(alphabetCountArr[i][xInt] > nowAlphabetCountArr[i][xInt]) { // 잊을 게 있으면
+				nowAlphabetCountArr[i][xInt]++;
+				nowCount[i]++;
+			}
+			
+			result += rememberCount[i] == nowCount[i] ? 1 : 0;
 		}
+		
+		return result;
 	}
 }
